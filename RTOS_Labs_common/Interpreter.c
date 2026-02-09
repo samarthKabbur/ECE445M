cbb4fa2389a300ff4d6f9bf89506711db1c5d71b
olyviawitham <olyviawitham4375@gmail.com>
2026-02-09 10:09:44 -0600

    lab2 interpreter.c-not checked

--- RTOS_Labs_common/Interpreter.c
+++ RTOS_Labs_common/Interpreter.c
@@ -16,7 +16,8 @@
 #include "../RTOS_Labs_common/eFile.h"
 #include "../RTOS_Labs_common/heap.h"
 #include "../RTOS_Labs_common/Interpreter.h"
-#include "../RTOS_Labs_common/RTOS_Debug_Prints.h"
+
+
 
 typedef struct{
   const char *name;
@@ -26,14 +27,18 @@ typedef struct{
 void Cmd_Flush(char* args, int l);
 void Cmd_Print(char* args, int l);
 void Cmd_Lab1_Results(char* args, int l);
+void Cmd_Lab2_Results(char* args, int l);
+void Cmd_DFT(char* args, int l);
+void Cmd_Jitter(char* args, int l);
 void Cmd_Help(char* args, int l);
-void Lab1_Results(uint32_t d);
-void Lab2(void);
 
 Command commands[] = {
   {"flush", Cmd_Flush},
   {"print", Cmd_Print},
   {"lab1results", Cmd_Lab1_Results},
+  {"lab2results", Cmd_Lab2_Results},
+  {"jitter", Cmd_Jitter},
+  {"dft", Cmd_DFT},
   {"?", Cmd_Help},
 };
 
@@ -48,10 +53,6 @@ ST7735_FillScreen(ST7735_BLACK);
 
 
 void Cmd_Print(char* args, int l){
-  if(args == 0){
-      UART_OutString("Error: No message provided\n");
-      return;
-  }
   UART_OutString(args);
   UART_OutChar('\n');
   ST7735_FillScreen(ST7735_BLACK);
@@ -60,27 +61,47 @@ void Cmd_Print(char* args, int l){
 
 
 void Cmd_Lab1_Results(char* args, int l){
-  // Lab2();
-  Lab1_Results(1);
+  //Lab1_Results(1);
   UART_OutString("\n");
   
-  
+}
+
+void Cmd_Lab2_Results(char* args, int l){
+  Lab2();
+  UART_OutString("\n");
   
 }
+
+void Cmd_DFT(char* args, int l){
+  DFT();
+  UART_OutString("\n");
+  
+}
+
+void Cmd_Jitter(char* args, int l){
+  Jitter();
+  UART_OutString("\n");
+  
+}
+
+
 void Cmd_Help(char* args, int l){
   UART_OutString("\r\nflush                 clears screen");
   UART_OutString("\r\nprint *message*       prints message onto screen");
   UART_OutString("\r\nlab1results           prints lab 1 results onto screen");
+  UART_OutString("\r\nlab2results           prints lab 2 results onto screen");
+  UART_OutString("\r\ndft                   prints lab 2 dft results onto screen");
+  UART_OutString("\r\njitter                prints lab 2  jitter results onto screen");
   UART_OutString("\r\n?                     displays syntax of all commands");
   UART_OutString("\n");
   ST7735_FillScreen(ST7735_BLACK);
-  ST7735_DrawString(0, l, "flush", ST7735_YELLOW);
-  l++;
-  ST7735_DrawString(0, l, "print *message*", ST7735_YELLOW);
-  l++;
-  ST7735_DrawString(0, l, "lab1results", ST7735_YELLOW);
-  l++;
-  ST7735_DrawString(0, l, "?", ST7735_YELLOW);
+  // ST7735_DrawString(0, l, "flush", ST7735_YELLOW);
+  // l++;
+  // ST7735_DrawString(0, l, "print *message*", ST7735_YELLOW);
+  // l++;
+  // ST7735_DrawString(0, l, "lab1results", ST7735_YELLOW);
+  // l++;
+  // ST7735_DrawString(0, l, "?", ST7735_YELLOW);
   
   
 
@@ -113,42 +134,62 @@ void ParseArgs(char *string, int l){
 
 }
 
-void Interpreter(void) {
-    char string[50];
-    int stringIndex = 0;
-    int l = 3; 
+//new Interpreter old one is below this just incase this doesnt work
+void Interpreter(void) {  
 
-    UART_OutChar(CARROT); 
 
-    while (1) {
-        uint8_t letter = UART_InChar();
+char string[50]; // didnt need to do this couldve just used InString(); will change later on
+int stringIndex = 0;
+int l = 3; //line number
 
-        if (letter == 13 || letter == 10) { 
-            if (stringIndex > 0) {
-                UART_OutChar(NEW_LINE);
-                string[stringIndex] = NULL_CHAR;
-                
-                ParseArgs(string, l);
-                
-                stringIndex = 0;
-                l++; 
-                UART_OutChar(CARROT);
-            }
-        }
-        else if (letter == 127 || letter == 8) {
-            if (stringIndex > 0) {
-                stringIndex--;
-                UART_OutChar(8);
-                UART_OutChar(' '); // Erase char
-                UART_OutChar(8);   // Move cursor back again
-            }
-        }
-        else {
-            if (stringIndex < 49) { // Leave room for NULL terminator
-                UART_OutChar(letter);
-                string[stringIndex] = letter; 
-                stringIndex++;
-            }
-        }
-    }
+UART_OutChar(CARROT); //begin by printing arrow
+
+  while (1) {                        // Loop forever
+
+
+  UART_InString(string, 50);
+  ParseArgs(string, l);
+   
+   
+
+  }
 }
+// void Interpreter(void) {
+//     char string[50];
+//     int stringIndex = 0;
+//     int l = 3; 
+
+//     UART_OutChar(CARROT); 
+
+//     while (1) {
+//         uint8_t letter = UART_InChar();
+
+//         if (letter == 13 || letter == 10) { 
+//             if (stringIndex > 0) {
+//                 UART_OutChar(NEW_LINE);
+//                 string[stringIndex] = NULL_CHAR;
+                
+//                 ParseArgs(string, l);
+                
+//                 stringIndex = 0;
+//                 l++; 
+//                 UART_OutChar(CARROT);
+//             }
+//         }
+//         else if (letter == 127 || letter == 8) {
+//             if (stringIndex > 0) {
+//                 stringIndex--;
+//                 UART_OutChar(8);
+//                 UART_OutChar(' '); // Erase char
+//                 UART_OutChar(8);   // Move cursor back again
+//             }
+//         }
+//         else {
+//             if (stringIndex < 49) { // Leave room for NULL terminator
+//                 UART_OutChar(letter);
+//                 string[stringIndex] = letter; 
+//                 stringIndex++;
+//             }
+//         }
+//     }
+// }
